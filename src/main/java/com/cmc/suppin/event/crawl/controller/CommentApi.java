fd51d37605b88576e7a2df4a3919e10682268be1
yxhwxn <officeyh9819@gmail.com>
2024-08-10 02:23:15 +0900

    Docs: Swagger API 명세 구체화

--- src/main/java/com/cmc/suppin/event/crawl/controller/CommentApi.java
+++ src/main/java/com/cmc/suppin/event/crawl/controller/CommentApi.java
@@ -6,6 +6,7 @@ import com.cmc.suppin.global.response.ApiResponse;
 import com.cmc.suppin.global.security.reslover.Account;
 import com.cmc.suppin.global.security.reslover.CurrentAccount;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -30,15 +31,13 @@ public class CommentApi {
 
     @GetMapping("/list")
     @Operation(summary = "크롤링된 전체 댓글 조회 API",
-            description = "주어진 이벤트 ID와 URL의 댓글을 페이지네이션하여 이벤트의 endDate 전에 작성된 댓글들만 조회합니다.<br><br>" +
-                    "Request: eventId: 조회할 이벤트의 ID, url: 댓글을 조회할 유튜브 URL, page: 조회할 페이지 번호 (1부터 시작), " +
-                    "size: 한 페이지당 댓글 수, Authorization: JWT 토큰을 포함한 인증 헤더<br>" +
-                    "Response: totalCommentCount: 전체 댓글 수, participantCount: 현재 페이지에서 가져온 댓글 수, crawlTime: 댓글 조회(크롤링) 요청 시간, comments: 각 댓글의 상세 정보 배열" +
-                    "author: 댓글 작성자, commentText: 댓글 내용, commentDate: 댓글 작성 시간")
+            description = "주어진 이벤트 ID와 URL의 댓글을 페이지네이션하여 이벤트의 endDate 전에 작성된 댓글들만 조회합니다. 자세한 요청 및 응답 형식은 노션 API 문서를 참고해주세요.")
     public ResponseEntity<ApiResponse<CommentResponseDTO.CrawledCommentListDTO>> getComments(
             @RequestParam Long eventId,
             @RequestParam String url,
+            @Parameter(description = "조회할 페이지 번호 (1부터 시작)")
             @RequestParam int page,
+            @Parameter(description = "한 페이지당 댓글 수")
             @RequestParam int size,
             @CurrentAccount Account account) {
         CommentResponseDTO.CrawledCommentListDTO comments = commentService.getComments(eventId, url, page, size, account.userId());
@@ -46,7 +45,7 @@ public class CommentApi {
     }
 
     @GetMapping("/draft-winners")
-    @Operation(summary = "조건별 당첨자 추첨 API", description = "주어진 조건에 따라 이벤트의 당첨자를 추첨합니다.")
+    @Operation(summary = "조건별 당첨자 추첨 API(댓글 이벤트)", description = "주어진 조건에 따라 이벤트의 당첨자를 추첨합니다.")
     public ResponseEntity<ApiResponse<CommentResponseDTO.WinnerResponseDTO>> drawWinners(
             @RequestParam Long eventId,
             @RequestParam String startDate,
--- src/main/java/com/cmc/suppin/event/crawl/controller/CrawlApi.java
+++ src/main/java/com/cmc/suppin/event/crawl/controller/CrawlApi.java
@@ -27,8 +27,12 @@ public class CrawlApi {
     @GetMapping("/comments/checkUrl")
     @Operation(summary = "크롤링 중복 검증 API",
             description = "주어진 URL과 eventId로 중복된 댓글 수집 이력이 있는지 확인합니다.<br><br>" +
-                    "Request: url: 중복 검증할 URL, eventId: 중복 검증할 이벤트 ID, Authorization: JWT 토큰을 포함한 인증 헤더<br>" +
-                    "Response: 중복된 댓글 수집 이력이 있을 경우 message 출력, 없을 경우 null")
+                    "Request<br>" +
+                    "- url: 중복 검증할 URL<br>" +
+                    "- eventId: 댓글 이벤트 생성 후 입력 받은 eventId<br><br>" +
+                    "Response<br>" +
+                    "- 요청된 URL과 중복된 댓글 수집 이력이 있을 경우 '검증 및 확인되었습니다.' 출력<br>" +
+                    "- 요청된 URL과 중복된 댓글 수집 이력이 없을 경우 '수집 이력이 없습니다.' 출력")
     public ResponseEntity<ApiResponse<String>> checkExistingComments(@RequestParam String url, @RequestParam Long eventId, @CurrentAccount Account account) {
         String message = crawlService.checkExistingComments(url, eventId, account.userId());
         if (message != null) {
@@ -40,9 +44,11 @@ public class CrawlApi {
     // 유튜브 댓글 크롤링(DB 저장)
     @PostMapping("/crawling/comments")
     @Operation(summary = "유튜브 댓글 크롤링 API",
-            description = "주어진 URL의 유튜브 댓글을 크롤링하여 DB에 저장합니다.<br><br>" +
+            description = "주어진 URL의 유튜브 댓글을 크롤링하여 해당 댓글 데이터를 DB에 저장합니다.<br><br>" +
                     "Request: url: 크롤링할 URL, eventId: 댓글을 수집할 eventId, forceUpdate: 댓글을 강제로 업데이트할지 여부(Boolean), Authorization: JWT 토큰을 포함한 인증 헤더 <br><br>" +
-                    "forceUpdate 입력 값이 false일 때 설명 <br> " +
+                    "<forceUpdate 입력 값이 true일 때> <br> " +
+                    "- 동일한 URL에 대한 댓글 크롤링 요청이지만, 강제로 업데이트하겠다는 의미이기 때문에, 기존 댓글 데이터를 삭제하고 새로 등록합니다. <br><br>" +
+                    "<forceUpdate 입력 값이 false일 때> <br> " +
                     "- DB에 기존 댓글이 존재하는 경우: 크롤링을 중지하고 예외를 던집니다. <br>" +
                     "- DB에 기존 댓글이 존재하지 않는 경우: 새로운 댓글을 크롤링하고 이를 DB에 저장합니다.")
     public ResponseEntity<ApiResponse<String>> crawlYoutubeComments(@RequestParam String url, @RequestParam Long eventId, @RequestParam boolean forceUpdate, @CurrentAccount Account account) {
